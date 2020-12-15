696edc724a1204da2e18abc0580b8ab97c060014
jmonagas <jmonagas@hotmail.ca>
2020-12-15 16:08:20 -0700

    Adding the files that are ready to be uploaded to the server.

--- serve.py
+++ serve.py
@@ -8,7 +8,28 @@ bjoern.run(app, host, port)
 
 
 
-# pm2 start /var/www/the-name-of-the-URL-goes-here/api/serve.py --interpreter "/var/www/the-name-of-the-URL-goes-here/api/venv/bin/python3.8" --name the-name-of-the-URL-goes-here
+# pm2 start /var/www/jmonagas-mvp-full-stack.ml/api/serve.py --interpreter "/var/www/jmonagas-mvp-full-stack.ml//api/venv/bin/python3.8" --name the-name-of-the-URL-goes-here
 
 
 # https://jmonagas-mvp-full-stack.ml/
+
+
+# <VirtualHost *:80>
+#   ServerName jmonagas-mvp-full-stack.ml
+#   ServerAlias www.jmonagas-mvp-full-stack.ml
+#   Redirect permanent / https://jmonagas-mvp-full-stack.ml
+# </VirtualHost>
+
+# <VirtualHost *:443>
+#   ServerName jmonagas-mvp-full-stack.ml
+#   ServerAlias www.jmonagas-mvp-full-stack.ml
+#   DocumentRoot "/var/www/jmonagas-mvp-full-stack.ml/mvp2dist"
+
+#   ProxyPreserveHost On
+#   ProxyPass /api http://127.0.0.1:5008/api
+#   ProxyPassReverse /api http://127.0.0.1:5008/api
+
+#   SSLEngine On
+#   SSLCertificateFile /etc/letsencrypt/live/jmonagas-mvp-full-stack.ml/fullchain.pem
+#   SSLCertificateKeyFile /etc/letsencrypt/live/jmonagas-mvp-full-stack.ml/privkey.pem
+# </VirtualHost>
